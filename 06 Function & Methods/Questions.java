6a3adde9f52fda4a0e358f9b51c2a42f59a7fd6f
Shamim Hasan <mxanaconda80@gmail.com>
2025-05-01 22:11:05 +0600

    Update Questions.java

--- 06 Function & Methods/Questions.java
+++ 06 Function & Methods/Questions.java
@@ -12,5 +12,18 @@ class Questions{
         }   
     }
     /*Questions 3 - Write a java program to check if a number is a palindrome in java?(121 is a palindrome 321 is not) */
+    public static void isPalindrome(int n){
+        int rev = 0, temp = n;
+        while(temp != 0){
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp /= 10;
+        }
+        if(rev == n){
+            System.out.println(n + " is a palindrome number.");
+        }else{
+            System.out.println(n + " is not a palindrome number.");
+        }
+    }
     public static void main(String[] args){}
 }
